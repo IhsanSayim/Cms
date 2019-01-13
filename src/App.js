64bdad51914caa4d9012a47f6fe5d8eee8f68f67
ihsansayim <ihsansaym@gmail.com>
2019-01-13 23:16:59 +0300

    Created Cms With React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './Components/List'
+import PersonList from './Components/PersonList'
+import Menu from './Components/Menu'
+import {Layout} from './Components/Layout'
+import { Route,Router } from 'react-router';
+import { Home } from './Components/Home';
+import { Category } from './Components/Category';
+import { Post } from './Components/Post';
+import { Deneme } from './Components/Deneme';
+import { BrowserRouter } from 'react-router-dom';
+import {PostDetail} from "./Components/PostDetail";
 
-class App extends Component {
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            items: []
+        };
+    }
+
+    onChange = (event) => {
+        this.setState({ term: event.target.value });
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            term: '',
+            items: [...this.state.items, this.state.term]
+        });
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+        <BrowserRouter>
+            <div>
+                <Layout/>
+            <Route exact path='/' component={Home} />
+            <Route path='/Category/:id' component={Category} />
+            <Route path='/Post/:id' component={PostDetail} />
+            <Route path='/deneme' component={Deneme} />
+            </div>
+        </BrowserRouter>
     );
   }
 }
